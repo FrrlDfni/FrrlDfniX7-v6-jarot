078c0ccca464e11640c370f2224f960a21b9dcd3
FrrlDfni <110106631+FrrlDfni@users.noreply.github.com>
2023-01-11 18:42:57 +0700

    Add files via upload

--- plugins/info-creator.js
+++ plugins/info-creator.js
@@ -55,7 +55,7 @@ const sentMsg  = await conn.sendMessage(
         }
     }
 )
-let jarot = 'https://telegra.ph/file/4cdb436effb280128325f.jpg'
+let jarot = 'https://telegra.ph/file/959b3f765e0089252dc38.jpg'
 await conn.send3ButtonImg(m.chat, jarot, `${ucapan()}\n\nHallo mypren, @${m.sender.split`@`[0]} 👋\nIni nomor owner botnya, jangan di spam ya\nKalau mau disave syarat nya harus pakai profile Sendiri\nKalau gak ada profile gak bakalan direspon\nJika penting langsung chat Owner\n\n⫹⫺ Date : *${week} ${date}*\n⫹⫺ Time : *${wib}*`, wm, 'Source', '.sc', 'Menu', '.menu', 'Donasi', '.donasi', sentMsg)}
 handler.help = ['owner']
 handler.tags = ['info']
--- plugins/tools-zodiac.js
+++ plugins/tools-zodiac.js
@@ -1,5 +1,5 @@
 let handler = (m, { usedPrefix, command, text }) => {
-    if (!text) throw `contoh:\n${usedPrefix + command} 2002 02 25`
+    if (!text) throw `contoh:\n${usedPrefix + command} 2004 09 29`
 
     const date = new Date(text)
     if (date == 'Invalid Date') throw date
@@ -22,7 +22,7 @@ Zodiak : ${zodiac}
 `.trim()
     m.reply(teks)
 }
-handler.help = ['zodiac *2002 02 25*']
+handler.help = ['zodiac *2004 09 29*']
 handler.tags = ['tools']
 
 handler.command = /^zodia[kc]$/i
